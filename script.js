0df3d42825e79a7d946db34d80ee2ecdbffe8d1c
matejbakala <117665159+bakalamatej@users.noreply.github.com>
2025-03-27 21:09:01 +0100

    Update script.js
    scroll animation

--- script.js
+++ script.js
@@ -34,3 +34,25 @@ document.querySelectorAll(".nav-menu a").forEach(link => {
         nav.classList.remove("show");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".section");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                    observer.unobserve(entry.target);
+                }
+            });
+        },
+        {
+            threshold: 0.2,
+        }
+    );
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
